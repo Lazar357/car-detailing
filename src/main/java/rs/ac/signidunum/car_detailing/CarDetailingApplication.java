f518cc1ccdd6c15e27975e803a1066c2446f68c6
Lazar357 <lazar.danic.23@singimail.rs>
2025-06-19 11:30:01 +0200

    Added basic frontend

---
+++ src/main/java/rs/ac/signidunum/car_detailing/CarDetailingApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.signidunum.car_detailing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDetailingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarDetailingApplication.class, args);
+	}
+
+}
