a55c82a59b843d36469d36634c511ee3b2c84e63
Lazar357 <lazar.danic.23@singimail.rs>
2025-07-01 21:33:36 +0200

    Edited frontend

--- src/main/java/rs/ac/signidunum/repo/CustomerRepository.java
+++ src/main/java/rs/ac/signidunum/repo/CustomerRepository.java
@@ -1,8 +1,6 @@
 package rs.ac.signidunum.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import rs.ac.signidunum.entity.Customer;
 
 import java.util.List;
